694a85074b405e15a04513229ccceb317fbb29b7
MarcosX <79601596+marco5x@users.noreply.github.com>
2023-05-13 20:21:39 -0300

    Update activities.js
    Update routes activities, method put and delete

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -5,9 +5,13 @@ Recibe los datos recolectados desde el formulario controlado de la ruta de
 creación de actividad turística por body
 Crea una actividad turística en la base de datos
 */
+server.get('/all', async (req, res) => {
+    const activities = await Activity.findAll({include:Country})
+    res.json(activities) 
+})
+
 server.post('/', async (req, res) => {
     const { name, difficulty, duration, season, countryId } = req.body
-    //console.log(req.body)
     try {
         const actTour = await Activity.create({
             name,
@@ -24,10 +28,33 @@ server.post('/', async (req, res) => {
     }  
 })
 
-server.get('/all', async (req, res) => {
-    const activities = await Activity.findAll({include:Country})
-    res.json(activities) 
+server.put('/:id', async(req, res) => {
+    const id = req.params.id
+	const { name, difficulty, duration, season } = req.body
+	try {
+		const activity = await Activity.findByPk(id)
+		await activity.update({
+			name: name || activity.dataValues.name,
+			difficulty: difficulty || activity.dataValues.difficulty,
+			duration: duration || activity.dataValues.duration,
+			season: season || activity.dataValues.season
+		})
+		activity? res.send(activity).status(200):res.sendStatus(400)		
+	} catch (error) {
+		res.send(error).status(404)
+	}
 })
 
+server.delete('/:id',async(req, res) => {
+    const id = req.params.id
+      try {
+          const activity = await Activity.destroy({
+              where: { id: id }
+          })
+          activity === 1 ? res.send('actividad eliminada satisfactoriamente') : res.send('actividad inexistente')
+      } catch (error) {
+          res.send(error).status(404)
+      }       
+})
 
 module.exports = server
